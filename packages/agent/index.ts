68144bc2fe8a29f7442b4cee2007b5676380b3d7
Rahmat Hidayat <yayatooo@MacBook-Air.local>
2026-09-09 11:56:38 +0700

    initialize model

--- packages/agent/index.ts
+++ packages/agent/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { generateCompletion } from "@anvia/core";
+import { model } from "./src/model";
+
+const res = await generateCompletion({
+  model,
+  prompt: "can u speak bahasa ?",
+  instructions: "you are a helpful assistant",
+});
+console.log(res.output);
+console.log(res.usage);
